570fa8176ac7b161cf64d3db130a0682a723dced
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-11 16:14:06 -0300

    Python

--- exercicios/005.py
+++
@@ -1,6 +0,0 @@
-"""
-Faça um Programa que converta metros para centímetros.
-"""
-metros = float(input("Digite o valor em metros: "))
-centimetros = metros * 100
-print(f"{metros:.2f} metros equivalem a {centimetros:.2f} centimetros.")
--- exercicios/040.py
+++
@@ -1,9 +0,0 @@
-"""
-Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
-Dica: utilize uma função de arredondamento.
-"""
-numero = float(input("Digite um número: "))
-if numero % 1 == 0:
-    print("Inteiro")
-else:
-    print("Decimal")
