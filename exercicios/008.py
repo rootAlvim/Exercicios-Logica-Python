d1f2c4dd174c5c1ebf880d519eff70434d6862c5
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-12 10:00:42 -0300

    Create 008.py

---
+++ exercicios/008.py
@@ -0,0 +1,4 @@
+ganho = float(input("Quanto você ganha por hora? ")) 
+trab = float(input("Quantas horas você trabalhou nesse mês? "))
+ganhomensal = ganho * trab
+print(f"Com base nas informações dadas seu salário neste mês é {ganhomensal}")
