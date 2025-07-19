b811a0df4c25155e69124c8fccda5094ca3e8f8e
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-19 15:31:05 -0300

    Create 022.py
    Python

---
+++ exercicios/022.py
@@ -0,0 +1,6 @@
+l = str(input("Digite uma  letra qualquer "))
+l =l.lower()
+if l == 'a' or l == 'e' or l == 'i' or l == 'o' or l == 'u':
+    print("A letra digitada é vogal")
+else:
+    print("A letra digitada não é uma vogal")
