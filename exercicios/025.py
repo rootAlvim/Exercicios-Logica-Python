dc5e5faa1e7331bae9a4cc602ac8c4f2d287c5a7
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-20 10:25:36 -0300

    Create 025.py
    python

---
+++ exercicios/025.py
@@ -0,0 +1,21 @@
+n1 = int(input("Digite o 1° valor: "))
+n2 = int(input("Digite o 2° valor: "))
+n3 = int(input("Digite o 3° valor: "))
+if n1 > n2 and n1 > n3:
+    maior = n1
+elif n2 > n1 and n2 > n3:
+    maior = n2
+elif n3 > n1 and n3 > n2:
+    maior = n3
+else:
+    maior = ' (Nenhum , todos os valores sao iguais)'
+print(f"O maior valor é {maior}")
+if n1 < n2 and n1 < n3:
+    menor = n1
+elif n2 < n1 and n2 < n3:   
+    menor = n2
+elif n3 < n1 and n3 < n2:
+    menor = n3
+else:
+    menor = ' (Nenhum , todos os valores sao iguais)'
+print(f"O menor valor é {menor}")       
