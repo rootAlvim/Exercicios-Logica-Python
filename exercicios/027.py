13e0bf42d1bf2728f720343aeca1f4592c7d5bd3
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-20 11:14:50 -0300

    Update 027.py
    python

--- exercicios/027.py
+++ exercicios/027.py
@@ -3,4 +3,4 @@ n2 = int(input("Digite o 2° valor: "))
 n3 = int(input("Digite o 3° valor: "))
 numeros = [n1, n2, n3]
 numeros.sort(reverse=True)
-print("Os valores em ordem decrescente são:", numeros)
+print("Os valores em ordem decrescente são:", numeros) 
