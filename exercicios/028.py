5f3d792db90b05bea16ab930bec4a8c5baeb6c85
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-07-20 11:16:50 -0300

    Create 028.py
    python

---
+++ exercicios/028.py
@@ -0,0 +1,7 @@
+turno = input("Digite o turno (M para matutino ou V para vespertino): ")
+if turno == "M":
+    print("Bom dia!")
+elif turno == "V":
+    print("Boa tarde!")
+else:
+    print("Turno inválido.")
