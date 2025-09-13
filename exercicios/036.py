45c72e5729a7c9bcec9b1ef585686f2ed28f5696
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-09-13 17:15:47 -0300

    Create 036.py

--- exercicios/036.py
+++ exercicios/036.py
@@ -1,15 +1,19 @@
 print("Me informe uma data qualquer no formato dd/mm/aaaa ")
-dia = int(input("Digite o dia:"))
-mês = int(input("Digite o mês:"))
-ano = str(input("Digite o ano:"))
-if dia > 31:
-    print("A data informada e inválida!")
-elif mês > 12:
-    print("A data informada e inválida!")
-elif ano[0] == '0':
-     print("A data informada e inválida!")
+
+dia = int(input("Digite o dia: "))
+mes = int(input("Digite o mês: "))
+ano = int(input("Digite o ano: "))
+
+# quantidade de dias em cada mês (considerando fevereiro = 28)
+dias_por_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# verifica ano bissexto
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    dias_por_mes[1] = 29  # fevereiro tem 29 dias
+
+if mes < 1 or mes > 12:
+    print("A data informada é inválida! (mês inexistente)")
+elif dia < 1 or dia > dias_por_mes[mes-1]:
+    print("A data informada é inválida! (dia fora do limite do mês)")
 else:
-     print("A data informada e válida!")
-
-
-
+    print("A data informada é válida!")
