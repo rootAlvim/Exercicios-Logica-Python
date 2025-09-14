1ef3625fd49d126b3a4b8f7d9953d4a223df1119
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-09-14 14:26:14 -0300

    Python

---
+++ 039.py
@@ -0,0 +1,7 @@
+n = int(input("Digite um número inteiro qualquer: "))
+if n < 0:
+    print("Número invalido")
+elif n % 2 == 0:
+    print("O número digitado e par")
+else:
+    print("O número digitado e ímpar")
--- a.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    int c = 9;
-    printf("%d", c);
-}
